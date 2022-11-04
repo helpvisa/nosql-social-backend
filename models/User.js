22c32112d2355f69993eea8ab531cba41ec4fe76
dan brackenbury <daniel.brackenbury@gmail.com>
2022-11-03 21:47:12 -0400

    fixed email validation

--- models/User.js
+++ models/User.js
@@ -3,8 +3,7 @@ const { Schema, model} = require('mongoose');
 
 // validators
 function validateEmail(email) {
-    const regex = new RegExp("/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/");
-    return regex.test(email);
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
 }
 
 // schemas
